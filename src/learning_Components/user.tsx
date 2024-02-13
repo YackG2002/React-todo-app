6c1557202e316291a1f113717bc604f482ea266f
Giovanni <yannick.houngbo.13@gmail.com>
2024-02-13 15:35:45 +0100

    feat: new gitignore adding

---
+++ src/learning_Components/user.tsx
@@ -0,0 +1,29 @@
+import React from "react"
+import { useState } from "react"
+
+type AuthUser = {
+    name: string,
+    email: string
+}
+
+export const User = () => {
+    const [user, setUser] = useState<AuthUser | null>(null);
+    const handleLogin = ()=>{
+        setUser({
+            name: "Yannick",
+            email: "yannick.houngbo.13@getDefaultNormalizer.com"
+        })
+    };
+    const handleLogout = ()=>{
+        setUser(null)
+    };
+
+    return(<>
+        <div>
+            <button onClick={handleLogin}>Login</button>
+            <button onClick={handleLogout}>Logout</button>
+            <div>Username is : {user?.name}</div>
+            <div>User email is : {user?.email}</div>
+        </div>
+    </>)
+}
